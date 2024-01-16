292c722fdcd9ef71ed7a5cbd29da7a26a603dada
Mahfuzul-Islam55 <shimulsust999@gmail.com>
2024-01-17 03:33:01 +0600

    feat: connecting with database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { datasourceOptions } from 'db/data-source';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(datasourceOptions)],
   controllers: [AppController],
   providers: [AppService],
 })
